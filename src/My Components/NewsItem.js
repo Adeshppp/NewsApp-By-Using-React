771ee1c547b8af1b3463cd8b829aba059df5ad38
Adesh <adeshp948@gmail.com>
2023-01-12 10:01:39 -0500

    I have added few properties and have added articles to news.js file

--- src/My Components/NewsItem.js
+++ src/My Components/NewsItem.js
@@ -3,21 +3,22 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
 
-  render() {
-    let {title,description}=this.props;
-    return (
-      <>
-      <div className="card" style={{width: "18rem"}}>
-  <img className="card-img-top" src="https://storage.googleapis.com/afs-prod/media/2dced0077df94935b04a07d10a13bf30/3000.jpeg" alt="Card image cap"/>
-  <div className="card-body">
-    <h5 className="card-title">{title}</h5>
-    <p className="card-text">{description}</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-      </>
-    )
-  }
+
+    render() {
+        let { title, description, imgUrl , newsUrl} = this.props;
+        return (
+            <>
+                <div className="card my-3" style={{ width: "18rem" }}>
+                    <img className="card-img-top" src={imgUrl} alt="Card image cap" />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <a href="/detailedNews" className="btn btn-sm btn-primary">Read More</a>
+                    </div>
+                </div>
+            </>
+        )
+    }
 }
 
 export default NewsItem
